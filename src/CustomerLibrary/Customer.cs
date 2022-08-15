c0d26c5a0aefaa7fa8ed3fde9d55667d2ec9af0e
CrashMaster8497 <crashmaster8497@gmail.com>
2022-08-15 21:11:52 +0800

    feat: entity classes

--- src/CustomerLibrary/Customer.cs
+++ src/CustomerLibrary/Customer.cs
@@ -2,6 +2,19 @@
 {
     public class Customer : Person
     {
+        public List<Address> AddressList { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public List<string> Notes { get; set; }
+        public decimal? TotalPurchasesAmount { get; set; }
 
+        public Customer(string firstName, string lastName, List<Address> addressList, string phoneNumber, string email, List<string> notes, decimal? totalPurchasesAmount) : base(firstName, lastName)
+        {
+            AddressList = addressList;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Notes = notes;
+            TotalPurchasesAmount = totalPurchasesAmount;
+        }
     }
 }
--- tests/CustomerLibrary.Tests/CustomerTest.cs
+++ tests/CustomerLibrary.Tests/CustomerTest.cs
@@ -2,6 +2,62 @@
 {
     public class CustomerTest
     {
+        [Fact]
+        public void ShouldHaveFirstName()
+        {
+            Customer customer = new Customer("first", "last", new List<Address>(), "phone", "email", new List<string>(), 0.0m);
 
+            Assert.Equal("first", customer.FirstName);
+        }
+
+        [Fact]
+        public void ShouldHaveLastName()
+        {
+            Customer customer = new Customer("first", "last", new List<Address>(), "phone", "email", new List<string>(), 0.0m);
+
+            Assert.Equal("last", customer.LastName);
+        }
+
+        [Fact]
+        public void ShouldHaveAddressList()
+        {
+            Customer customer = new Customer("first", "last", new List<Address>(), "phone", "email", new List<string>(), 0.0m);
+
+            Assert.NotNull(customer.AddressList);
+            Assert.IsType<List<Address>>(customer.AddressList);
+        }
+
+        [Fact]
+        public void ShouldHavePhoneNumber()
+        {
+            Customer customer = new Customer("first", "last", new List<Address>(), "phone", "email", new List<string>(), 0.0m);
+
+            Assert.Equal("phone", customer.PhoneNumber);
+        }
+
+        [Fact]
+        public void ShouldHaveEmail()
+        {
+            Customer customer = new Customer("first", "last", new List<Address>(), "phone", "email", new List<string>(), 0.0m);
+
+            Assert.Equal("email", customer.Email);
+        }
+
+        [Fact]
+        public void ShouldHaveNotes()
+        {
+            Customer customer = new Customer("first", "last", new List<Address>(), "phone", "email", new List<string>(), 0.0m);
+
+            Assert.NotNull(customer.Notes);
+            Assert.IsType<List<string>>(customer.Notes);
+        }
+
+        [Fact]
+        public void ShouldHaveTotalPurchasesAmount()
+        {
+            Customer customer = new Customer("first", "last", new List<Address>(), "phone", "email", new List<string>(), 0.0m);
+
+            Assert.Equal(0.0m, customer.TotalPurchasesAmount);
+        }
     }
 }
